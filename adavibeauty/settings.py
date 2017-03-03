3ca5a0ec48e5684095e0d65316723d5f30e28566
Dmitry Danilov <Dmitry Danilov>
2017-03-04 00:51:15 +0200

    Added new model's structure to products

--- adavibeauty/settings.py
+++ adavibeauty/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'adavibeauty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adavibeauty',
+        'USER': 'postgres',
+        'PASSWORD': 'NumberOne123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,176 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Category model
+    """
+    title = models.CharField(max_length=50, unique=True)
+    is_active = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    """
+    Product body
+    """
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    is_available = models.BooleanField()
+    status = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
+
+
+class ProductVariation(models.Model):
+    """
+    Variations of every product.
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    vendor_code = models.CharField(max_length=20, unique=True)
+    price = models.FloatField()
+    color = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    rate = models.FloatField()
+    is_new = models.BooleanField()
+    is_available = models.BooleanField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Product variation"
+        verbose_name_plural = "Products variations"
+
+    def __str__(self):
+        return str(self.product) + " - " + self.vendor_code
+
+
+def upload_path(instance, filename):
+    """
+    Path to files
+    :param instance:
+    :param filename:
+    :return:
+    """
+    return "products/{0}".format(filename)
+
+
+class ProductImage(models.Model):
+    """
+    Store of images for all products
+    """
+    product = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
+    image = models.FileField(upload_to=upload_path)
+    description = models.CharField(null=True, blank=True, max_length=200)
+    is_main = models.BooleanField()
+    main_product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = "Product image"
+        verbose_name_plural = "Products images"
+
+    def __str__(self):
+        return self.product
+
+
+class ProductReview(models.Model):
+    """
+    Reviews of every product
+    """
+    product = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
+    review = models.TextField()
+    reviewer_name = models.CharField(max_length=100)
+    reviewer_email = models.EmailField(max_length=254)
+
+    class Meta:
+        verbose_name = "Product Review"
+        verbose_name_plural = "Products Reviews"
+
+    def __str__(self):
+        return self.reviewer_name + " - " + self.reviewer_email
+
+
+class Order(models.Model):
+    """
+    Order model. Stores detail information of every order
+    """
+    billing_first_name = models.CharField(max_length=100)
+    billing_last_name = models.CharField(max_length=100)
+    shipping_first_name = models.CharField(max_length=100)
+    shipping_last_name = models.CharField(max_length=100)
+    shipping_type = models.CharField(max_length=50)
+    shipping_street = models.CharField(max_length=200, null=True)
+    shipping_state = models.CharField(max_length=100, null=True)
+    shipping_postcode = models.CharField(max_length=15, null=True)
+    shipping_country = models.CharField(max_length=50, null=True)
+    shipping_city = models.CharField(max_length=100, null=True)
+    shipping_departament = models.CharField(max_length=400, null=True)
+    shipping_to_home = models.BooleanField()
+    shipping_phone = models.CharField(max_length=20)
+    shipping_email = models.EmailField(max_length=254)
+
+    shipping_total = models.FloatField()
+    discount_code = models.CharField(max_length=20, null=True)
+    total = models.FloatField()
+    item_total = models.IntegerField()
+    pay_status = models.BooleanField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+        ordering = ["-id"]
+
+    def billing_name(self):
+        return "%s %s" % (self.billing_first_name, self.billing_last_name)
+
+    def __str__(self):
+        return "%s %s %s" % (self.id, self.billing_name(), self.time)
+
+
+class OrderItem(models.Model):
+    """
+    Order item model. Stores an information about every product in order
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Order item"
+        verbose_name_plural = "Order items"
+
+
+class Discount(models.Model):
+    """
+    Discount model. Stores an information about discount in details
+    """
+    discount_title = models.CharField(max_length=200)
+    discount_range = models.IntegerField()
+    discount_code = models.CharField(max_length=20)
+    discount_type = models.CharField(max_length=100)
+    discount_start_period = models.DateTimeField()
+    discount_end_period = models.DateTimeField()
+    discount_description = models.TextField()
+
+    class Meta:
+        verbose_name = "Discount"
+        verbose_name_plural = "Discounts"
+
+    def __str__(self):
+        return "%s %s" % (self.discount_code, self.discount_type)
+
