8b5f226e3f70a60ed0e58afe525f5163911e6118
Dmitry Danilov <Dmitry Danilov>
2017-03-17 00:41:22 +0200

    Modified settings.py (added default database 'db.sqlite3')

--- adavibeauty/settings.py
+++ adavibeauty/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'adavibeauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'adavibeauty',
+#         'USER': 'postgres',
+#         'PASSWORD': 'NumberOne123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'adavibeauty',
-        'USER': 'postgres',
-        'PASSWORD': 'NumberOne123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
