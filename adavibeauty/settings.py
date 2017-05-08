6297bc3b8c0455c9f7b542988e2292976921ce93
Dmitry Danilov <Dmitry Danilov>
2017-05-08 13:51:06 +0300

    Fixed page-proofs of snackbars; added alert for 403 code; added cleaning of form, when status 200.

--- adavibeauty/settings.py
+++ adavibeauty/settings.py
@@ -276,12 +276,12 @@ LOCALE_PATHS = (
 
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    '/static/',
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     '/static/',
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
