e7db54a65790a5ea5ff81053bf8bdf73ad93689d
Danilov Dmitry <danilovdmitry94@gmail.com>
2017-07-14 14:55:24 +0300

    Fixed: price filter (slider) in products

--- static/price.js
+++ static/price.js
@@ -1,5 +1,10 @@
 var price = {
     set: function (min_price, max_price) {
+        if (+min_price >= +max_price) {
+            min_price = +max_price - 10
+        } else if (+max_price <= +min_price) {
+            max_price = +min_price + 10
+        }
         return $.post(PRURL.setPrice, {min_price: min_price, max_price: max_price}, function (xhr) {
             // console.log("min " + min_price);
             // console.log("max " + max_price);
