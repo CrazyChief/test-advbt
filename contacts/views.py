97f59ef32ea1e3e4f4b44e354d094deefa8f23b0
Danilov Dmitry <danilovdmitry94@gmail.com>
2017-07-18 17:17:31 +0300

    Added: email sending to contacts app; fixed: subscribers template.

--- contacts/views.py
+++ contacts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy as _
-from .models import Review, Location, Email, Phone
+
+from .models import Location, Email, Phone
 from .forms import ReviewForm
 from products.models import Category
+from templated_email.generic_views import TemplatedEmailFormViewMixin
 
 
-class ReviewCreate(CreateView):
+class ReviewCreate(TemplatedEmailFormViewMixin, CreateView):
     template_name = 'contacts/index.html'
     form_class = ReviewForm
+    templated_email_template_name = 'contact_noreply'
 
     def get_context_data(self, **kwargs):
         context = super(ReviewCreate, self).get_context_data(**kwargs)
@@ -20,4 +23,12 @@ class ReviewCreate(CreateView):
 
     def get_success_url(self):
         return _('contacts:index')
+
+    def templated_email_get_recipients(self, form):
+        return [form.data['email']]
+
+    def templated_email_get_context_data(self, **kwargs):
+        context = super(ReviewCreate, self).templated_email_get_context_data(**kwargs)
+        context['full_name'] = self.request.POST['name']
+        return context
 
