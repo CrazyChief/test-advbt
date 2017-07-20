00cff89e8993ccb077a81a532a360376a3848f68
Danilov Dmitry <danilovdmitry94@gmail.com>
2017-07-20 18:27:14 +0300

    Replaced: some functions

--- payment/signals.py
+++ payment/signals.py
@@ -1,28 +1,48 @@
 from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
 from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received
+from paypal.standard.ipn.signals import valid_ipn_received, invalid_ipn_received
 from orders.models import Order
 
 
 print("We in signals file. Defining of function")
 
 
+@receiver(valid_ipn_received)
+@receiver(invalid_ipn_received)
+# def payment_notification(sender, **kwargs):
+#     ipn_obj = sender
+#     print(sender)
+#     if ipn_obj.payment_status == ST_PP_COMPLETED:
+#         print("Order completed! :)")
+#         print("PK: %s" % ipn_obj.invoice)
+#         # if ipn_obj.receiver_email != 'danilovdmitry94-facilitator-1@gmail.com':
+#         if ipn_obj['receiver_email'] != 'seller@paypalsandbox.com':
+#             print("No receiver email! :(")
+#             return
+#
+#         order = get_object_or_404(Order, pk=ipn_obj.invoice)
+#         # if order is not None:
+#         print(order)
+#         order.pay_status = True
+#         order.save()
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
     print(sender)
-    if ipn_obj.payment_status == ST_PP_COMPLETED:
+    if ipn_obj['payment_status'] == ST_PP_COMPLETED:
         print("Order completed! :)")
-        print("PK: %s" % ipn_obj.invoice)
-        if ipn_obj.receiver_email != 'danilovdmitry94-facilitator-1@gmail.com':
+        print("PK: %s" % ipn_obj['invoice'])
+        if ipn_obj['receiver_email'] != 'seller@paypalsandbox.com':
+        # 'danilovdmitry94-facilitator-1@gmail.com'
             print("No receiver email! :(")
             return
 
-        order = get_object_or_404(Order, pk=ipn_obj.invoice)
+        order = get_object_or_404(Order, pk=ipn_obj['invoice'])
         # if order is not None:
         print(order)
         order.pay_status = True
         order.save()
 
-print("We defined function.")
+# print("We defined function.")
 valid_ipn_received.connect(payment_notification)
-print("We awake our function!)")
+# print("We awake our function!)")
--- payment/views.py
+++ payment/views.py
@@ -43,11 +43,46 @@ def PaymentProcess(request):
     }
     return render(request, 'payment/process.html', context)
 
+
+from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.ipn.signals import valid_ipn_received
+from payment.signals import payment_notification
+from orders.models import Order
+
+
+# print("We in signals file. Defining of function")
+
+
+# def payment_notification(sender, **kwargs):
+#     ipn_obj = sender
+#     print(sender)
+#     if ipn_obj['payment_status'] == ST_PP_COMPLETED:
+#         print("Order completed! :)")
+#         print("PK: %s" % ipn_obj['invoice'])
+#         if ipn_obj['receiver_email'] != 'seller@paypalsandbox.com':
+#         # 'danilovdmitry94-facilitator-1@gmail.com'
+#             print("No receiver email! :(")
+#             return
+#
+#         order = get_object_or_404(Order, pk=ipn_obj['invoice'])
+#         # if order is not None:
+#         print(order)
+#         order.pay_status = True
+#         order.save()
+
+# print("We defined function.")
+# valid_ipn_received.connect(payment_notification)
+# print("We awake our function!)")
+
+
 @csrf_exempt
 def PaymentDone(request):
+    valid_ipn_received.connect(payment_notification)
+    print(request.POST)
     cart = Cart(request)
     cart.empty()
     return render(request, 'payment/done.html')
+
 
 @csrf_exempt
 def PaymentCanceled(request):
